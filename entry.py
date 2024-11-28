f1dc01cedc8db21f216e8acaa2e86af980508e05
souhaiebtar <tarhounisouhaieb@gmail.com>
2024-11-28 13:10:49 +0100

    Build a URL in Flask

--- entry.py
+++ entry.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+
+@app.route('/admin')  # decorator for route(argument) function
+def hello_admin():  # binding to hello_admin call
+    return 'Hello Admin'
+
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):  # binding to hello_guest call
+    return 'Hello %s as Guest' % guest
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':  # dynamic binding of URL to function
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest=name))
 
 
 if __name__ == '__main__':
